2149233f00afba08b74a8290c2c3c3cc913a2046
Geziel Carvalho <geziel.natal@gmail.com>
2023-07-21 12:20:40 -0400

    create, update and delete category

--- backend/src/main/java/info/gezielcarvalho/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/info/gezielcarvalho/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,19 @@
 package info.gezielcarvalho.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import info.gezielcarvalho.dscatalog.dto.CategoryDTO;
 import info.gezielcarvalho.dscatalog.services.CategoryService;
@@ -32,4 +38,32 @@ public class CategoryResource {
 		}
 		return ResponseEntity.ok().body(service.findOne(id));
 	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO category){
+		var result = service.create(category);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(result.getId()).toUri();
+		return ResponseEntity.created(uri).body(result);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO category) {
+		var result = service.findOne(id);
+		if (result == null) {
+			return ResponseEntity.notFound().build();
+		}
+		CategoryDTO updated = service.save(id,category);
+		return ResponseEntity.ok().body(updated);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
+		var result = service.findOne(id);
+		if (result == null) {
+			return ResponseEntity.notFound().build();
+		}
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- backend/src/main/java/info/gezielcarvalho/dscatalog/services/CategoryService.java
+++ backend/src/main/java/info/gezielcarvalho/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import info.gezielcarvalho.dscatalog.dto.CategoryDTO;
+import info.gezielcarvalho.dscatalog.entities.Category;
 import info.gezielcarvalho.dscatalog.repositories.CategoryRepository;
 
 @Service
@@ -26,11 +27,33 @@ public class CategoryService {
 	            .collect(Collectors.toList());
 	}
 
+	@Transactional(readOnly = true)
 	public CategoryDTO findOne(Long id) {
 		var result = repository.findById(id);
 		if (result.isPresent()) {
 			return new CategoryDTO(result.get());
 		}
 		return null;
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO create(CategoryDTO category) {
+		var result = repository
+				.save(new Category(category.getId(),category.getName()));
+		return new CategoryDTO(result);
+	}
+
+	@Transactional
+	public CategoryDTO save(Long id, CategoryDTO category) {
+		Category entity = repository.getReferenceById(id);
+		entity.setName(category.getName());
+		var result = repository
+				.save(entity);
+		return new CategoryDTO(result);
+	}
+
+	public void delete(Long id) {
+		// after adding product entity, check referential integrity 
+		repository.deleteById(id);
 	}
 }
