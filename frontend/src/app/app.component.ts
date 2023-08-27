20569c29283ece54d4d5511065bfa1680e0e26cc
Geziel Carvalho <geziel.natal@gmail.com>
2023-08-27 15:13:05 -0400

    separete category component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Category } from './models/category.model';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +7,4 @@ import { Category } from './models/category.model';
 })
 export class AppComponent {
   title = 'DS Catalog';
-  categories: Category[] = [
-    { id: 1, name: 'Books' },
-    { id: 2, name: 'Electronics' },
-    { id: 3, name: 'Computers' },
-    { id: 4, name: 'Magazines' },
-  ];
-
-  create(name: string) {
-    let id = this.categories.length + 1;
-    let newCategory: Category = { id, name };
-    this.categories.push(newCategory);
-  }
-
-  destroy(id: number) {
-    if (confirm('Are you sure you want to delete this category?')) {
-      this.categories = this.categories.filter(
-        (category) => category.id !== id
-      );
-    }
-  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryModule } from './components/category/category.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CategoryModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
