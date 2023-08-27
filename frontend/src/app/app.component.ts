8ffebb0449485eced29c5b86aea076d57828f378
Geziel Carvalho <geziel.natal@gmail.com>
2023-08-26 23:18:46 -0400

    transfer mock data to logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'DS Catalog';
+  categories = [
+    { id: 1, name: 'Books' },
+    { id: 2, name: 'Electronics' },
+    { id: 3, name: 'Computers' },
+    { id: 4, name: 'Magazines' },
+  ];
 }
