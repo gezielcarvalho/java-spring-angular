6ee3c16d995c2b484108b66042c3ad77cfff668c
Geziel Carvalho <geziel.natal@gmail.com>
2023-08-29 10:37:28 -0400

    create badge

--- frontend/src/app/components/category/category.module.ts
+++ frontend/src/app/components/category/category.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatBadgeModule } from '@angular/material/badge';
 
 import { CategoryComponent } from './category.component';
 
@@ -16,6 +17,7 @@ import { CategoryComponent } from './category.component';
     MatFormFieldModule,
     MatInputModule,
     MatAutocompleteModule,
+    MatBadgeModule,
     ReactiveFormsModule,
     NgFor,
     AsyncPipe,
