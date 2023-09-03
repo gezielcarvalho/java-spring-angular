68393907e85a133e19b687856b324897eeea2e05
Geziel Carvalho <geziel.natal@gmail.com>
2023-09-03 18:30:29 -0400

    create category item

---
+++ frontend/src/app/components/category/category-list/category-item/category-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-item',
+  templateUrl: './category-item.component.html',
+  styleUrls: ['./category-item.component.css']
+})
+export class CategoryItemComponent {
+
+}
