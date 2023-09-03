c030a424cde6071bfbe6ef564473ee9af93ca627
Geziel Carvalho <geziel.natal@gmail.com>
2023-09-03 18:29:38 -0400

    customize tailwind style

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "tw-", // tailwind prefix
-  //important: true,  tailwind important
+  //prefix: "tw-",  tailwind prefix
+  //important: true, // tailwind important,
   content: ["./src/**/*.{html,ts}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+        gray: {
+          100: "#f2f3f5",
+          200: "#ebedef",
+          300: "#e3e5e8",
+          400: "#d4d7dc",
+          600: "#4f545c",
+          700: "#36393f",
+          800: "#2f3136",
+          900: "#202225",
+        },
+      },
+    },
   },
   plugins: [],
 };
