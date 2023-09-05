622a9080e2b7339c99afa7cd1081f37b5b8b3b87
Geziel Carvalho <geziel.natal@gmail.com>
2023-09-04 21:22:21 -0400

    simplify model

--- frontend/src/app/models/category.model.ts
+++ frontend/src/app/models/category.model.ts
@@ -1,21 +1,8 @@
-// In the models/category.model.ts file
-
 export class Category {
-  id: number;
-  name: string;
-  description: string;
-  imageUrl?: string;
-  // Add other properties and methods as needed
-
   constructor(
-    id: number,
-    name: string,
-    description: string,
-    imageUrl?: string
-  ) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.imageUrl = imageUrl;
-  }
+    public id: number,
+    public name: string,
+    public description: string,
+    public imageUrl?: string
+  ) {}
 }
