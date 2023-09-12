717d6a7dc8bd950572ff69964a09f271a727a39b
Geziel Carvalho <geziel.natal@gmail.com>
2023-09-12 19:11:13 -0400

    create directives

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,4 +6,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DS Catalog';
+
+  ngOnInit() {
+    console.log('ngOnInit AppComponent');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck AppComponent');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit AppComponent');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked AppComponent');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit AppComponent');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked AppComponent');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy AppComponent');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges AppComponent');
+  }
+
+  onActivate() {
+    console.log('onActivate AppComponent');
+  }
+
+  onDeactivate() {
+    console.log('onDeactivate AppComponent');
+  }
+
+  onInit() {
+    console.log('onInit AppComponent');
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,42 +1,45 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { AddCategoryModalComponent } from './screens/category/add-category-modal/add-category-modal.component';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './screens/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CategoryComponent } from './screens/category/category.component';
+import { CategoryDetailComponent } from './screens/category/category-detail/category-detail.component';
+import { CategoryItemComponent } from './screens/category/category-list/category-item/category-item.component';
+import { CategoryListComponent } from './screens/category/category-list/category-list.component';
+import { CategoryToolbarComponent } from './screens/category/category-toolbar/category-toolbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './screens/home/home.component';
 import { LogoComponent } from './components/header/logo/logo.component';
-import { ModalComponent } from './components/header/modal/modal.component';
 import { NavigationComponent } from './components/header/navigation/navigation.component';
 import { SidebarComponent } from './components/header/sidebar/sidebar.component';
 import { SidebarIconComponent } from './components/header/sidebar/sidebar-icon/sidebar-icon.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { CategoryItemComponent } from './screens/category/category-list/category-item/category-item.component';
-import { CategoryListComponent } from './screens/category/category-list/category-list.component';
-import { CategoryComponent } from './screens/category/category.component';
-import { CategoryDetailComponent } from './screens/category/category-detail/category-detail.component';
-import { CategoryToolbarComponent } from './screens/category/category-toolbar/category-toolbar.component';
-import { AddCategoryModalComponent } from './screens/category/add-category-modal/add-category-modal.component';
+import { BasicHighlightDirective } from './directives/basic-highlight.directive';
+import { BetterHighlightDirective } from './directives/better-highlight.directive';
 
 @NgModule({
   declarations: [
+    AddCategoryModalComponent,
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
+    CategoryComponent,
+    CategoryDetailComponent,
     CategoryItemComponent,
     CategoryListComponent,
-    CategoryComponent,
+    CategoryToolbarComponent,
+    HomeComponent,
+    HeaderComponent,
     LogoComponent,
-    ModalComponent,
     NavigationComponent,
     SidebarComponent,
     SidebarIconComponent,
-    CategoryDetailComponent,
-    CategoryToolbarComponent,
-    AddCategoryModalComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
   ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, FormsModule],
+  imports: [AppRoutingModule, BrowserModule, FontAwesomeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
