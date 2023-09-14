04d95bc5eb987e71260d858c578dacdd184172d1
Geziel Carvalho <geziel.natal@gmail.com>
2023-09-13 21:50:00 -0400

    order imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddCategoryModalComponent } from './screens/category/add-category-modal/add-category-modal.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BasicHighlightDirective } from './directives/basic-highlight.directive';
+import { BetterHighlightDirective } from './directives/better-highlight.directive';
 import { CategoryComponent } from './screens/category/category.component';
 import { CategoryDetailComponent } from './screens/category/category-detail/category-detail.component';
 import { CategoryItemComponent } from './screens/category/category-list/category-item/category-item.component';
 import { CategoryListComponent } from './screens/category/category-list/category-list.component';
+import { CategoryService } from './services/category.service';
 import { CategoryToolbarComponent } from './screens/category/category-toolbar/category-toolbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './screens/home/home.component';
@@ -18,11 +21,8 @@ import { LogoComponent } from './components/header/logo/logo.component';
 import { NavigationComponent } from './components/header/navigation/navigation.component';
 import { SidebarComponent } from './components/header/sidebar/sidebar.component';
 import { SidebarIconComponent } from './components/header/sidebar/sidebar-icon/sidebar-icon.component';
-import { BasicHighlightDirective } from './directives/basic-highlight.directive';
-import { BetterHighlightDirective } from './directives/better-highlight.directive';
 import { UnlessDirective } from './directives/unless.directive';
 import { UserDropdownComponent } from './components/header/navigation/user-dropdown/user-dropdown.component';
-import { CategoryService } from './services/category.service';
 
 @NgModule({
   declarations: [
