92a03edbeaa332da30604ad2a7c81964a37ab149
Geziel Carvalho <geziel.natal@gmail.com>
2023-09-16 06:05:04 -0400

    remove unused code

--- frontend/src/app/screens/category/category-list/category-item/category-item.component.ts
+++ frontend/src/app/screens/category/category-list/category-item/category-item.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Category } from 'src/app/models/category.model';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category-item',
   templateUrl: './category-item.component.html',
 })
 export class CategoryItemComponent {
+  constructor(private categoryService: CategoryService) {}
   @Input() category: Category | undefined;
+  onSelected() {
+    this.categoryService.categorySelected.emit(this.category);
+  }
 }
--- frontend/src/app/screens/category/category.component.ts
+++ frontend/src/app/screens/category/category.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
 
 import { Observable } from 'rxjs/internal/Observable';
-import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category',
@@ -11,33 +9,11 @@ import { map, startWith } from 'rxjs/operators';
 })
 export class CategoryComponent {
   filteredOptions: Observable<string[]> | undefined;
-  allowNewCategory = false;
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter((option) =>
-      option.toLowerCase().includes(filterValue)
-    );
-  }
-
   myControl = new FormControl('');
   edtImage = new FormControl('');
   strImageUrl = 'Lance';
   options: string[] = ['One', 'Two', 'Three'];
   public date: Date = new Date();
 
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.date = new Date(this.route.snapshot.params['date']);
-    this.edtImage.setValue('');
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value || ''))
-    );
-
-    setTimeout(() => {
-      this.allowNewCategory = true;
-    }, 10000);
-  }
+  constructor() {}
 }
